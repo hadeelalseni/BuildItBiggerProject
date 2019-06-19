c8e73adf2fb079e8b57ebf195b52ec4155c22bc0
hadeelalseni <hadeelalseni@gmail.com>
2019-06-19 13:57:30 +0300

    test commit

--- app/src/main/java/test/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/main/java/test/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -13,6 +13,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 //@RunWith(AndroidJUnit4.class)
+
+//@RunWith(AndroidJUnit4.class)
+
 public class EndpointsAsyncTaskTest extends AndroidTestCase {
 
     public class MyTest {
